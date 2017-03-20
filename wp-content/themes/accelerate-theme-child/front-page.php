3b7a3420c3a069d1c2bfdba9b16e5c9edad795a7
xybandit <nicole@thebashfulcreative.com>
2017-03-20 04:09:55 -0400

    added theme staging

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -21,7 +21,22 @@ get_header(); ?>
 				<a class="button" href="<?php echo home_url(); ?>/blog">View Our Work</a>
 			</div>
 		<?php endwhile; // end of the loop. ?>
-	</div><!-- .container -->
+    </div><!-- .container -->
 </section><!-- .home-page -->
+<!-- blog post -->
+<section class="recent-posts">
+    <div class="site-content">
+        <div class="blog-post">
+            <h4>From the Blog</h4>
+    <?php query_posts('posts_per_page=1'); ?>
+     <?php while ( have_posts() ) : the_post(); ?>
+       <h2><?php the_title(); ?></h2>
+       <?php the_excerpt(); ?> 
+       <a class="read-more-link" href="<?php the_permalink(); ?>">Read More <span>&rsaquo;</span></a>
+     <?php endwhile; ?> 
+    <?php wp_reset_query(); ?>
+        </div>
+    </div>
+</section>
 
 <?php get_footer(); ?>
